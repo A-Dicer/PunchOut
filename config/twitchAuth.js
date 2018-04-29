09372969eaed0432f778e8b771f6a2c033c2d960
Andrew Dicer <andrew.dicer@gmail.com>
2018-04-28 21:34:47 -0500

    heroku

---
+++ config/twitchAuth.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'twitchAuth' : {
+        'clientID'      : 'gesdgsmo8v3xgtwevfpuwsw1x1oyqa',
+        'clientSecret'  : 'lg7yxsr3ijdr90gosnix0tsb8ucb5v', 
+        'callbackURL'   : 'http://localhost:3002/api/auth/twitch/callback',
+        'scope'         : 'user_read'
+    },
+};
