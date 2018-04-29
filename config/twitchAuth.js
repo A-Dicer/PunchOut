8b22003f344c72e8b5e0ec9e98710f8ba3502341
Andrew Dicer <andrew.dicer@gmail.com>
2018-04-29 14:34:59 -0500

    heroku

--- config/twitchAuth.js
+++ config/twitchAuth.js
@@ -2,7 +2,7 @@ module.exports = {
     'twitchAuth' : {
         'clientID'      : 'gesdgsmo8v3xgtwevfpuwsw1x1oyqa',
         'clientSecret'  : 'lg7yxsr3ijdr90gosnix0tsb8ucb5v', 
-        'callbackURL'   : 'https://cryptic-falls-49392.herokuapp.com/api/auth/twitch/callback',
+        'callbackURL'   : 'https://punch-out.herokuapp.com/api/auth/twitch/callback',
         'scope'         : 'user_read'
     },
 };
